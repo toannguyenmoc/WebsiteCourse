b14a9487ab08c79bca12e3f92df1f467f272c215
Kaylin Tran <ttknganct@gmail.com>
2025-05-29 13:09:40 +0700

    add custom component

--- src/components/Client/ClientFooter.vue
+++ src/components/Client/ClientFooter.vue
@@ -1,13 +1,69 @@
 <template>
-    <div>
+    <footer class="w-100 ftco-footer ftco-no-pt">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-md-3 pt-5">
+                    <div class="ftco-footer-widget pt-md-5 mb-4">
+                        <h2 class="ftco-heading-2">Giới thiêu</h2>
+                        <p>Trang web cung cấp các khóa học IT chất lượng, giúp bạn nâng cao kỹ năng lập trình, phát
+                            triển phần mềm và công nghệ thông tin từ cơ bản đến nâng cao.</p>
+                        <ul class="ftco-footer-social list-unstyled float-md-left float-lft">
+                            <li class=""><a href="#"><span class="fa fa-twitter"></span></a></li>
+                            <li class=""><a href="#"><span class="fa fa-facebook"></span></a></li>
+                            <li class=""><a href="#"><span class="fa fa-instagram"></span></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 pt-5">
+                    <div class="ftco-footer-widget pt-md-5 mb-4 ml-md-5">
+                        <h2 class="ftco-heading-2">Trợ giúp</h2>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="py-2 d-block">Chăm sóc khách hàng</a></li>
+                            <li><a href="#" class="py-2 d-block">Trợ giúp pháp lý</a></li>
+                            <li><a href="#" class="py-2 d-block">Dịch vụ</a></li>
+                            <li><a href="#" class="py-2 d-block">Quyền riêng tư & chính sách</a></li>
+                            <li><a href="#" class="py-2 d-block">Chính sách hoàn tiền</a></li>
+                            <li><a href="#" class="py-2 d-block">Liên hệ</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 pt-5">
+                    <div class="ftco-footer-widget pt-md-5 mb-4">
+                        <h2 class="ftco-heading-2">Khóa học</h2>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="py-2 d-block">Computer Engineering</a></li>
+                            <li><a href="#" class="py-2 d-block">Web Design</a></li>
+                            <li><a href="#" class="py-2 d-block">Data Analysis</a></li>
+                            <li><a href="#" class="py-2 d-block">AI</a></li>
+                            <li><a href="#" class="py-2 d-block">Computer Technician</a></li>
+                            <li><a href="#" class="py-2 d-block">Web Developer</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 pt-5">
+                    <div class="ftco-footer-widget pt-md-5 mb-4">
+                        <h2 class="ftco-heading-2">Thông tin liên hệ</h2>
+                        <div class="block-23 mb-3">
+                            <ul>
+                                <li><span class="icon fa fa-map-marker"></span><span class="text">Toà nhà FPT
+                                        Polytechnic, đường số 22, phường Thường Thạnh, quận Cái Răng, TP Cần Thơ </span>
+                                </li>
+                                <li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+84 392 392
+                                            210</span></a></li>
+                                <li><a href="#"><span class="icon fa fa-paper-plane"></span><span
+                                            class="text">info@yourdomain.com</span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-    </div>
+        </div>
+    </footer>
+
 </template>
 
-<script setup>
+<script>
 
 </script>
-
-<style lang="scss" scoped>
-
-</style>
+<style></style>
--- src/components/Common/PaginationCustom.vue
+++ src/components/Common/PaginationCustom.vue
@@ -1,13 +1,71 @@
 <template>
-    <div>
+    <div class="row mt-5">
+        <div class="col">
+            <div class="block-27">
+                <ul>
+                    <!-- previous -->
+                    <li><a class="me-2" href="#" @click.prevent="goToPage(currentPage - 1)"
+                            :class="{ 'disabled': currentPage === 1 }">&lt;</a></li>
+                    <!-- pages number -->
+                    <li v-for="page in visiblePages" class="me-2" :key="page" :class="{ active: currentPage === page }">
+                        <a class="me-2" href="#" @click.prevent="goToPage(page)">{{ page }} </a>
+                    </li>
 
+                    <!-- next -->
+                    <li><a  href="#">&gt;</a></li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
-
 <script setup>
+import { computed } from 'vue'
+const props = defineProps({
+    currentPage: {
+        type: Number,
+        default: 1
+    },
+    totalItems: {
+        type: Number,
+        required: true
+    },
+    pageSize: {
+        type: Number,
+        default: 10
+    },
+    maxVisibleButtons: {
+        type: Number,
+        default: 5
+    }
+});
+
+const emit = defineEmits(['update:currentPage']);
+
+const totalPages = computed(() => Math.ceil(props.totalItems / props.pageSize));
+
+const goToPage = (page) => {
+  if (page >= 1 && page <= totalPages.value && page !== props.currentPage) {
+    emit('update:currentPage', page)
+  }
+}
+
+const visiblePages = computed(() => {
+    const pages = []
+    let start = Math.max(1, props.currentPage - Math.floor(props.maxVisibleButtons / 2))
+    let end = start + props.maxVisibleButtons - 1
+
+    if (end > totalPages.value) {
+        end = totalPages.value
+        start = Math.max(1, end - props.maxVisibleButtons + 1)
+    }
+
+    for (let i = start; i <= end; i++) {
+        pages.push(i)
+    }
+    return pages
+});
 
 </script>
-
-<style lang="scss" scoped>
+<style >
 
 </style>
