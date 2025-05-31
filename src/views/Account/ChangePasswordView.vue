ae76a67bc187b0ef0936415022de18f7020aef7f
MinhPC09533 <nguyenquangminh3316@gmail.com>
2025-05-31 12:34:01 +0700

    AddLogin Register forgotpassword information changePassword

--- src/views/Account/ChangePasswordView.vue
+++ src/views/Account/ChangePasswordView.vue
@@ -1,13 +1,82 @@
 <template>
-    <div>
-
+    <section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-12">
+					<div class="wrapper">
+						<div class="row no-gutters">
+							<div class="col-lg-8 col-md-7 order-md-last d-flex align-items-stretch">
+								<div class="contact-wrap w-100 p-md-5 p-4">
+									<h3 class="mb-4">Change Password</h3>
+									<form method="POST" id="contactForm" name="contactForm" class="contactForm">
+										<div class="row">
+											<div class="col-md-12">
+												<div class="form-group">
+													<label class="label" for="subject">Email</label>
+													<input type="text" class="form-control" name="subject" id="subject" placeholder="fptpolytechnic@gmail.com" disabled>
+												</div>
+											</div>
+											<div class="col-md-12">
+												<div class="form-group">
+													<label class="label" for="subject">Old Password</label>
+													<input type="text" class="form-control" name="subject" id="subject" placeholder="">
+												</div>
+											</div>
+                                            <div class="col-md-12">
+												<div class="form-group">
+													<label class="label" for="subject">New Password</label>
+													<input type="text" class="form-control" name="subject" id="subject" placeholder="">
+												</div>
+											</div>
+											  <div class="col-md-12">
+												<div class="form-group">
+													<label class="label" for="subject">Confirm Password</label>
+													<input type="text" class="form-control" name="subject" id="subject" placeholder="">
+												</div>
+											</div>
+											<div class="col-md-12">
+												<div class="form-group">
+													<input type="submit" value="Update" class="btn btn-primary">
+													<div class="submitting"></div>
+												</div>
+											</div>
+										</div>
+									</form>
+								</div>
+							</div>
+							<div class="col-lg-4 col-md-5 d-flex align-items-stretch">
+								<div class="info-wrap bg-primary w-100 p-md-5 p-4">
+									<div class="col-lg-4 col-md-5 d-flex align-items-stretch">
+<div class="col-lg-4 col-md-5 d-flex align-items-stretch">
+    <div class="info-wrap bg-primary w-100 p-md-5 p-4 text-center m-sm--5">
+        <div class="mb-4 d-flex align-items-center justify-content-center rounded-circle bg-white border border-white mx-auto" style="width: 200px; height: 200px;">
+            <i class="bi bi-person" style="font-size: 200px; color: #6c757d;"></i>
+        </div>
+       
     </div>
+</div>
+
+
+</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-12 mt-5">
+					<div id="map" class="bg-white"></div>
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
 
-<script setup>
+<script>
 
 </script>
 
 <style lang="scss" scoped>
+
 
 </style>
