b06dac01c6acd352df054d5ecbc98e551ce1b5cc
Kaylin Tran <ttknganct@gmail.com>
2025-06-17 13:37:14 +0700

    [BE] add middleware

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const TOKEN = 'TOKEN';
+export const Role = 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role';
+export const Email = 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name';
+export const UserId = 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier';
