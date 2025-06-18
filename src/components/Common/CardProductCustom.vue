7bff8396f9bb90483fbcb19952e902c507d39a5c
TriTai2004 <nguyentritai24042004@gmail.com>
2025-06-18 16:44:01 +0700

    [FE] add

--- src/components/Common/CardProductCustom.vue
+++ src/components/Common/CardProductCustom.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="project-wrap">
-        <RouterLink :to="`/course-detail/${slug}`" class="img" :style="{ backgroundImage: `url(${image})`}" >
+        <RouterLink :to="`/course-detail/${slug}`" @click="setCourseId" class="img" :style="{ backgroundImage: `url(${image})`}" >
             <span class="price">{{ category }}</span>
         </RouterLink>
         <div class="text p-4">
-            <h3><RouterLink :to="`/course-detail/${slug}`">{{ title }}</RouterLink></h3>
+            <h3><RouterLink :to="`/course-detail/${slug}`" @click="setCourseId">{{ title }}</RouterLink></h3>
             <p class="advisor">Giáo viên <span>{{ advisor }}</span></p>
             <ul class="d-flex justify-content-between">
                 <li><span class="flaticon-shower"><span class="fa fa-user"></span></span>{{ students }}</li>
@@ -14,18 +14,22 @@
     </div>
 </template>
 <script setup>
-defineProps({
+const props = defineProps({
   image: String,
   title: String,
   advisor: String,
   students: Number,
   price: Number,
   category: String,
-  slug: String
+  slug: String, 
+  id: Number
 });
 
 const formatPrice = (value) => {
   return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(value);
+};
+const setCourseId = () => {
+  sessionStorage.setItem("courseId", props.id);
 };
 
 </script>
