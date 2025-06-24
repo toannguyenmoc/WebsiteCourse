5ad92c0c8fbbf3e5f681d3010e04730adc9b3dce
TriTai2004 <nguyentritai24042004@gmail.com>
2025-06-24 11:49:44 +0700

    [FE] update courseDetail Lesson Report

--- src/views/Client/ClientCourseDetailView.vue
+++ src/views/Client/ClientCourseDetailView.vue
@@ -40,8 +40,11 @@
                         <span>Tác giả: {{ useCourse.accountTeacher }}</span>
                     </div>
                     <hr>
-                    <button v-if="!check && useCourse.price != 0" @click="payment" class="btn btn-primary">Đăng ký ngay</button>
-                    <RouterLink v-if="check || useCourse.price === 0" :to="lessons.length > 0 ? `/lesson/${lessons[0].id}` : `/course-detail/${useCourse.slug}`" class="btn btn-primary">Vào học ngay</RouterLink>
+                    <button v-if="!check && useCourse.price != 0" @click="payment" class="btn btn-primary">Đăng ký
+                        ngay</button>
+                    <RouterLink v-if="check || useCourse.price === 0"
+                        :to="lessons.length > 0 ? `/lesson/${lessons[0].id}` : `/course-detail/${useCourse.slug}`"
+                        class="btn btn-primary">Vào học ngay</RouterLink>
                 </div>
             </div>
         </div>
@@ -50,7 +53,7 @@
         <div class="description mt-3 mx-0 py-2 bg-white text-dark">
             {{ useCourse.description }}
         </div>
-        <div class="lesson mt-3 mx-0 pb-5  bg-white rounded-3" id="lesson">
+        <div class="lesson mt-3 mx-0 pb-2  bg-white rounded-3" id="lesson">
             <h4>Nội dung khóa học</h4>
             <div class="d-flex gap-5">
                 <p>{{ lessons.length }} bài học</p>
@@ -92,6 +95,52 @@
                 </div>
             </div>
         </div>
+        <div class="container p-2">
+            <div class="card p-3">
+                <h4 >Đánh giá</h4>
+                <div class="px-4 d-flex pb-3">
+                    <h3 class="mr-2 my-auto">{{ Number(useCourse.totalRating).toFixed(1) }}</h3>
+                    <!-- <i v-for="item in 5" :key="item" class="fa fa-star text-yellow"></i> -->
+                    <i class="my-auto h1 fa fa-star text-yellow"></i>
+                </div>
+                <hr>
+                <form v-if="check || useCourse.price === 0" @submit.prevent="createComment" class="p-4">
+                    <div class="form-group">
+                        <textarea v-model="comment" class="form-control rounded" placeholder="Đánh giá của bạn" style="resize: none;" rows="4" name="" id=""></textarea>
+                    </div>
+                    <div class="form-group d-flex">
+                        <div class="rating">
+                            <i v-for="star in 5" :key="star" class="fa fa-star"
+                                :class="{ selected: star <= selectedRating, hover: star <= hoverRating }"
+                                @mouseover="hoverRating = star"
+                                @mouseleave="hoverRating = 0"
+                                @click="selectedRating = star"></i>
+                        </div>
+                        <button :disabled="comment && !selectedRating" class="ml-auto btn btn-primary">Gửi đánh giá</button>
+                    </div>
+                </form>
+                <div class="">
+                    <div v-for="item in listCommentByCourse" :key="item.id" class="mb-4">
+                        <div class="d-flex">
+                            <div class="avatar mr-2 my-auto">
+                            <img :src="item.image || 'https://hoseiki.vn/wp-content/uploads/2025/03/avatar-mac-dinh-4.jpg'"
+                                alt="avatar">
+                        </div>
+                        <div class="">
+                            <span>{{ item.nameAccount }}</span>
+                            <div>
+                                <small class="mr-3">{{ formatDate(item.commentedDate) }} {{ timeAgo(item.commentedDate) }}</small>
+                                <i v-for="index in item.rating" class="my-auto h1 fa fa-star text-yellow"></i>
+                            </div>
+                        </div>
+                        </div>
+                        <span class="ml-1">{{ item.comment }}</span>
+                    </div>
+                    
+                    <hr>
+                </div>
+            </div>
+        </div>
     </div>
 
 
@@ -102,15 +151,21 @@ import { ref, onMounted, watch } from 'vue';
 import { useCourses } from '@/composables/useCourses';
 import { getData, postData } from '@/services/apiMiddleware';
 import { useRoute, useRouter, } from 'vue-router';
-import { showError, showSuccess } from '@/assets/Admin/js/alert';
+import { showError, showSuccess, showAlert } from '@/assets/Admin/js/alert';
+import { getUserId } from '@/utils/getUserIdUtils';
+import { formatDate, timeAgo } from '@/utils/dateUtils';
 const lessons = ref([]);
 const route = useRoute();
 const router = useRouter();
 const id = ref(0);
 const useCourse = ref({});
 const check = ref(false);
-///
-const accountId = sessionStorage.getItem('accountId');
+const selectedRating = ref(0);
+const hoverRating = ref(0);
+const comment = ref('');
+const apiComment = "/comment";
+const listCommentByCourse = ref([]);
+const accountId = ref(0);
 let paymentStatus = route.query.vnp_TransactionStatus;
 
 const {
@@ -127,9 +182,15 @@ watch(course, () => {
 onMounted(async () => {
     try {
         id.value = sessionStorage.getItem("courseId");
+        accountId.value = getUserId();
         await fetchCourseById(id.value);
         const data = await getData("/lesson", { courseId: id.value, page: 0, size: 999 });
         lessons.value = data.data.filter(ls => ls.status).sort((a, b) => a.lesson - b.lesson);
+        await getCommentByCourse();
+        if (!accountId.value) {
+            check.value = false;
+            return;
+        }
         await checkCoursePayment();
         if (paymentStatus) {
             await create();
@@ -144,11 +205,15 @@ onMounted(async () => {
 const generateOrderId = () => {
     const timestamp = Date.now(); // thời gian tính bằng ms
     const random = Math.floor(Math.random() * 1000);
-    //Them account id
-    return `ORDER_${1}_${useCourse.value.id}_${timestamp}${random}`;
+    return `ORDER_${accountId.value}_${useCourse.value.id}_${timestamp}${random}`;
 }
 
 const payment = async () => {
+    if (!accountId.value) {
+        await showAlert("Đăng nhập để tiếp tục!");
+        router.push('/login');
+        return;
+    }
     const amount = useCourse.value.price - (useCourse.value.discount || 0);
     const orderInfo = generateOrderId();
     try {
@@ -172,11 +237,11 @@ const create = async () => {
             accountId: route.query.vnp_OrderInfo.split("_")[1],
             courseId: route.query.vnp_OrderInfo.split("_")[2],
         }
-        const res = await postData('/payment' + window.location.search,data);;
+        const res = await postData('/payment' + window.location.search, data);;
         if (res) {
             await checkCoursePayment();
             await showSuccess('Thanh toán thành công!');
-        }else{
+        } else {
             await showError('Thanh toán thất bại!')
         }
         await router.push('/course-detail/' + useCourse.value.slug);
@@ -188,13 +253,48 @@ const create = async () => {
 const checkCoursePayment = async () => {
     try {
         const result = await getData('payment/check/course', {
-            accountId: 1,
+            accountId: accountId.value,
             courseId: id.value
         });
         check.value = result;
     } catch (error) {
         console.error("Lỗi kiểm tra thanh toán khóa học:", error);
         check.value = false;
+    }
+}
+
+const createComment = async () =>{
+    if(!accountId.value){
+        return;
+    }
+    try {
+        const data = {
+            rating: selectedRating.value,
+            parentComment: null,
+            comment: comment.value,
+            accountId: accountId.value,
+            courseId: id.value
+        }
+        const res = await postData(apiComment, data);
+        selectedRating.value = 0;
+        comment.value = '';
+        await getCommentByCourse();
+        await fetchCourseById(id.value);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getCommentByCourse = async()=>{
+    const data = {
+        courseId: id.value
+    }
+    try {
+        const res = await getData(`${apiComment}/by-course`, data);
+        listCommentByCourse.value = res;
+
+    } catch (error) {
+        console.log(error);
     }
 }
 
@@ -291,5 +391,22 @@ const formatPrice = (value) => {
     -webkit-box-orient: vertical !important;
     overflow: hidden !important;
     text-overflow: ellipsis !important;
+}
+.rating {
+  display: inline-flex;
+  direction: ltr;
+}
+.rating i {
+  font-size: 1.4rem;
+  color: #ccc;
+  cursor: pointer;
+  transition: color 0.2s;
+  margin-right: 2px;
+}
+.rating i.hover {
+  color: #ffc107;
+}
+.rating i.selected {
+  color: #ffc107;
 }
 </style>
